efd3cc6f2d1f2508dfb96a4ac6cc58b234a40421
Josh <joshdanielrea@gmail.com>
2022-02-03 09:41:15 -0600

    none commit

--- holyfoods/models.py
+++ holyfoods/models.py
@@ -1,3 +1,21 @@
+from logging.config import _LoggerConfiguration
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.Charfield(max_length=100)
+    photo_url = models.Textfield(max_length=200)
+
+    def __str__(self):
+        return self.start_year
+
+
+class Donut(models.Model):
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, related_name='donuts')
+    name = models.CharField(max_length=100)
+    details = models.TextField(max_length=200, default='no detials')
+    image_url = models.TextField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
