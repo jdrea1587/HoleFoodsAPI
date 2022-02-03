1b5f7d16ab81cee74f911ec7a1193d521f7a62c8
RicoWilliams <ricowilliams@protonmail.com>
2022-02-03 08:35:00 -0700

    admin set up, models showing in admin panel

--- holyfoods/admin.py
+++ holyfoods/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Shop
+from .models import Donut
+admin.site.register(Shop)
+admin.site.register(Donut)
--- holyfoods/models.py
+++ holyfoods/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    photo_url = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Donut(models.Model):
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, related_name='donuts')
+    name = models.CharField(max_length=100, default='no name')
+    details = models.CharField(max_length=100, default='no details')
+    image_url = models.TextField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
